e2dcc103c8b31a74afc137cfe0afec09dc4f37be
Salman2h <32548459+Salman2h@users.noreply.github.com>
2017-10-06 12:41:30 -0400

    Add files via upload

--- Rana-Aujeszky-Gregory-Decrypt/UserInput.Designer.cs
+++ Rana-Aujeszky-Gregory-Decrypt/UserInput.Designer.cs
@@ -28,99 +28,54 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.readDictionaryButton = new System.Windows.Forms.Button();
-            this.inputTextbox = new System.Windows.Forms.TextBox();
-            this.outputTextBox = new System.Windows.Forms.TextBox();
             this.cipherLabel = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.cipherTextBox = new System.Windows.Forms.TextBox();
             this.plainTextBox = new System.Windows.Forms.TextBox();
             this.plainTextLabel = new System.Windows.Forms.Label();
-            this.checkExistanceLabel = new System.Windows.Forms.Label();
-            this.resultLabel = new System.Windows.Forms.Label();
             this.decryptButton = new System.Windows.Forms.Button();
-            this.maxLengthWordButton = new System.Windows.Forms.Button();
-            this.minLengthWordButton = new System.Windows.Forms.Button();
+            this.lblDelimiter = new System.Windows.Forms.Label();
+            this.cbDelimiter = new System.Windows.Forms.ComboBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.lblCaption = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // readDictionaryButton
-            // 
-            this.readDictionaryButton.Location = new System.Drawing.Point(256, 552);
-            this.readDictionaryButton.Name = "readDictionaryButton";
-            this.readDictionaryButton.Size = new System.Drawing.Size(155, 23);
-            this.readDictionaryButton.TabIndex = 0;
-            this.readDictionaryButton.Text = "IsExists";
-            this.readDictionaryButton.UseVisualStyleBackColor = true;
-            this.readDictionaryButton.Click += new System.EventHandler(this.ReadDictionaryButton_Click);
-            // 
-            // inputTextbox
-            // 
-            this.inputTextbox.Location = new System.Drawing.Point(256, 436);
-            this.inputTextbox.Name = "inputTextbox";
-            this.inputTextbox.Size = new System.Drawing.Size(208, 20);
-            this.inputTextbox.TabIndex = 1;
-            // 
-            // outputTextBox
-            // 
-            this.outputTextBox.Location = new System.Drawing.Point(256, 503);
-            this.outputTextBox.Name = "outputTextBox";
-            this.outputTextBox.Size = new System.Drawing.Size(208, 20);
-            this.outputTextBox.TabIndex = 2;
             // 
             // cipherLabel
             // 
             this.cipherLabel.AutoSize = true;
-            this.cipherLabel.Location = new System.Drawing.Point(171, 68);
+            this.cipherLabel.Location = new System.Drawing.Point(21, 38);
             this.cipherLabel.Name = "cipherLabel";
             this.cipherLabel.Size = new System.Drawing.Size(65, 13);
             this.cipherLabel.TabIndex = 3;
             this.cipherLabel.Text = "Enter Cipher";
             // 
-            // textBox1
+            // cipherTextBox
             // 
-            this.textBox1.Location = new System.Drawing.Point(265, 68);
-            this.textBox1.Multiline = true;
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(375, 128);
-            this.textBox1.TabIndex = 4;
+            this.cipherTextBox.Location = new System.Drawing.Point(24, 54);
+            this.cipherTextBox.Multiline = true;
+            this.cipherTextBox.Name = "cipherTextBox";
+            this.cipherTextBox.Size = new System.Drawing.Size(375, 91);
+            this.cipherTextBox.TabIndex = 4;
             // 
             // plainTextBox
             // 
-            this.plainTextBox.Location = new System.Drawing.Point(265, 237);
+            this.plainTextBox.Location = new System.Drawing.Point(24, 223);
             this.plainTextBox.Multiline = true;
             this.plainTextBox.Name = "plainTextBox";
-            this.plainTextBox.Size = new System.Drawing.Size(375, 128);
+            this.plainTextBox.Size = new System.Drawing.Size(375, 54);
             this.plainTextBox.TabIndex = 6;
             // 
             // plainTextLabel
             // 
             this.plainTextLabel.AutoSize = true;
-            this.plainTextLabel.Location = new System.Drawing.Point(171, 237);
+            this.plainTextLabel.Location = new System.Drawing.Point(21, 207);
             this.plainTextLabel.Name = "plainTextLabel";
             this.plainTextLabel.Size = new System.Drawing.Size(54, 13);
             this.plainTextLabel.TabIndex = 5;
             this.plainTextLabel.Text = "Plain Text";
             // 
-            // checkExistanceLabel
-            // 
-            this.checkExistanceLabel.AutoSize = true;
-            this.checkExistanceLabel.Location = new System.Drawing.Point(88, 436);
-            this.checkExistanceLabel.Name = "checkExistanceLabel";
-            this.checkExistanceLabel.Size = new System.Drawing.Size(148, 13);
-            this.checkExistanceLabel.TabIndex = 7;
-            this.checkExistanceLabel.Text = "Check Existance in Dictionary";
-            // 
-            // resultLabel
-            // 
-            this.resultLabel.AutoSize = true;
-            this.resultLabel.Location = new System.Drawing.Point(171, 503);
-            this.resultLabel.Name = "resultLabel";
-            this.resultLabel.Size = new System.Drawing.Size(37, 13);
-            this.resultLabel.TabIndex = 8;
-            this.resultLabel.Text = "Found";
-            // 
             // decryptButton
             // 
-            this.decryptButton.Location = new System.Drawing.Point(347, 389);
+            this.decryptButton.Location = new System.Drawing.Point(244, 163);
             this.decryptButton.Name = "decryptButton";
             this.decryptButton.Size = new System.Drawing.Size(155, 23);
             this.decryptButton.TabIndex = 9;
@@ -128,43 +83,64 @@
             this.decryptButton.UseVisualStyleBackColor = true;
             this.decryptButton.Click += new System.EventHandler(this.decryptButton_Click);
             // 
-            // maxLengthWordButton
+            // lblDelimiter
             // 
-            this.maxLengthWordButton.Location = new System.Drawing.Point(438, 552);
-            this.maxLengthWordButton.Name = "maxLengthWordButton";
-            this.maxLengthWordButton.Size = new System.Drawing.Size(187, 23);
-            this.maxLengthWordButton.TabIndex = 10;
-            this.maxLengthWordButton.Text = "Max Length Plain Text";
-            this.maxLengthWordButton.UseVisualStyleBackColor = true;
-            this.maxLengthWordButton.Click += new System.EventHandler(this.MaxLengthWordButton_Click);
+            this.lblDelimiter.AutoSize = true;
+            this.lblDelimiter.Location = new System.Drawing.Point(21, 168);
+            this.lblDelimiter.Name = "lblDelimiter";
+            this.lblDelimiter.Size = new System.Drawing.Size(47, 13);
+            this.lblDelimiter.TabIndex = 12;
+            this.lblDelimiter.Text = "Delimiter";
             // 
-            // minLengthWordButton
+            // cbDelimiter
             // 
-            this.minLengthWordButton.Location = new System.Drawing.Point(643, 552);
-            this.minLengthWordButton.Name = "minLengthWordButton";
-            this.minLengthWordButton.Size = new System.Drawing.Size(187, 23);
-            this.minLengthWordButton.TabIndex = 11;
-            this.minLengthWordButton.Text = "Min Length Plain Text";
-            this.minLengthWordButton.UseVisualStyleBackColor = true;
-            this.minLengthWordButton.Click += new System.EventHandler(this.minLengthWordButton_Click);
+            this.cbDelimiter.FormattingEnabled = true;
+            this.cbDelimiter.Items.AddRange(new object[] {
+            "Comma",
+            "Tab",
+            "Space"});
+            this.cbDelimiter.Location = new System.Drawing.Point(74, 165);
+            this.cbDelimiter.Name = "cbDelimiter";
+            this.cbDelimiter.Size = new System.Drawing.Size(149, 21);
+            this.cbDelimiter.TabIndex = 13;
+            this.cbDelimiter.Text = "Comma";
+            this.cbDelimiter.SelectedIndexChanged += new System.EventHandler(this.cbDelimiter_SelectedIndexChanged);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(137, 292);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(155, 23);
+            this.btnClose.TabIndex = 14;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // lblCaption
+            // 
+            this.lblCaption.AutoSize = true;
+            this.lblCaption.Font = new System.Drawing.Font("Microsoft YaHei UI", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCaption.Location = new System.Drawing.Point(92, 9);
+            this.lblCaption.Name = "lblCaption";
+            this.lblCaption.Size = new System.Drawing.Size(251, 24);
+            this.lblCaption.TabIndex = 15;
+            this.lblCaption.Text = "Permutation Cryptanalysis";
+            this.lblCaption.Click += new System.EventHandler(this.label1_Click);
             // 
             // UserInput
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(900, 625);
-            this.Controls.Add(this.minLengthWordButton);
-            this.Controls.Add(this.maxLengthWordButton);
+            this.ClientSize = new System.Drawing.Size(416, 327);
+            this.Controls.Add(this.lblCaption);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.cbDelimiter);
+            this.Controls.Add(this.lblDelimiter);
             this.Controls.Add(this.decryptButton);
-            this.Controls.Add(this.resultLabel);
-            this.Controls.Add(this.checkExistanceLabel);
             this.Controls.Add(this.plainTextBox);
             this.Controls.Add(this.plainTextLabel);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.cipherTextBox);
             this.Controls.Add(this.cipherLabel);
-            this.Controls.Add(this.outputTextBox);
-            this.Controls.Add(this.inputTextbox);
-            this.Controls.Add(this.readDictionaryButton);
             this.Name = "UserInput";
             this.Text = "Rana-Aujeszky-Gregory-Decrypt";
             this.ResumeLayout(false);
@@ -173,19 +149,15 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button readDictionaryButton;
-        private System.Windows.Forms.TextBox inputTextbox;
-        private System.Windows.Forms.TextBox outputTextBox;
         private System.Windows.Forms.Label cipherLabel;
-        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox cipherTextBox;
         private System.Windows.Forms.TextBox plainTextBox;
         private System.Windows.Forms.Label plainTextLabel;
-        private System.Windows.Forms.Label checkExistanceLabel;
-        private System.Windows.Forms.Label resultLabel;
         private System.Windows.Forms.Button decryptButton;
-        private System.Windows.Forms.Button maxLengthWordButton;
-        private System.Windows.Forms.Button minLengthWordButton;
+        private System.Windows.Forms.Label lblDelimiter;
+        private System.Windows.Forms.ComboBox cbDelimiter;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label lblCaption;
     }
 }
 
--- Rana-Aujeszky-Gregory-Decrypt/UserInput.cs
+++ Rana-Aujeszky-Gregory-Decrypt/UserInput.cs
@@ -1,5 +1,8 @@
 ﻿using System;
+using System.Collections;
+
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
@@ -13,52 +16,76 @@ namespace Rana_Aujeszky_Gregory_Decrypt
 {
     public partial class UserInput : Form
     {
-
-        private List<string> fileItems;
+        private char delimiter=',';
+        private List<string> plainTextList;
+        private Hashtable keyTable;
+        private Hashtable freqList;
         public UserInput()
         {
             InitializeComponent();
-
-            ReadFile readFile = new ReadFile();
-            
-            fileItems = readFile.LoadDictionary();
-        }
-
-        private bool IsStringExistInDictionary(string searchString)
-        {
-            bool isStringExists = false;
-            if (fileItems.Find(delegate (string matchingString) { return matchingString == searchString; }) != null)
-            {
-                isStringExists = true;
-            }
-
-            return isStringExists;
-        }
-
-        private void ReadDictionaryButton_Click(object sender, EventArgs e)
-        {
-            if (IsStringExistInDictionary(inputTextbox.Text))
-            {
-                outputTextBox.Text = "Found";
-            }
-            else
-            {
-                outputTextBox.Text = "Not Found";
-            }
+            plainTextList = Helper.LoadDictionary("..//..//Test 1 Candidate Plaintexts.txt");
+            keyTable = Helper.InitKeys();
+            freqList = Helper.InitFrequency();
         }
         
-
-        private void MaxLengthWordButton_Click(object sender, EventArgs e)
-        {
-            outputTextBox.Text = fileItems.OrderByDescending(s => s.Length).First();
-        }
-
-        private void minLengthWordButton_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void decryptButton_Click(object sender, EventArgs e)
+        {   
+            string[] cipherText = cipherTextBox.Text.Split(delimiter);
+
+            foreach (string plainText in plainTextList)
+            {
+                foreach (string key in keyTable.Keys)
+                {
+                    ((ArrayList)keyTable[key]).Clear();
+                }
+
+                bool isMatched = true;
+                if (plainText.Length == cipherText.Length)
+                {
+                    for (int index = 0; index < cipherText.Length && isMatched; index++)
+                    {
+                        string aKey = plainText.Substring(index, 1);
+                        ArrayList aList = ((ArrayList)keyTable[aKey]);
+                        if (aList.Count >= (int)freqList[aKey])
+                        {
+                            isMatched = false;
+                        }
+                        else
+                        {
+                            aList.Add(cipherText[index]);
+                        }
+                    }
+                    if (isMatched)
+                    {
+                        plainTextBox.Text += string.Format("{0}{1}", (plainTextBox.Text.Length > 0) ? "," : "", plainText);
+                        break;
+                    }
+                }
+            }
+        }
+
+        private void cbDelimiter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (cbDelimiter.Text)
+            {
+                case "Tab":
+                    delimiter = '\t';
+                    break;
+                case "Comma":
+                    delimiter = ',';
+                    break;
+                case "Space":
+                    delimiter = ' ';
+                    break;
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
         {
 
         }
